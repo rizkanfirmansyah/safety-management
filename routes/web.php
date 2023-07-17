2ec19b1fac0bcbeec7c440625ec86f15ef1897ac
rizkanfirmansyah <riezkanaprianda@gmail.com>
2023-07-17 23:09:36 +0700

    add route login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\OrganitationController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
@@ -21,4 +22,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index']);
 Route::resource('users', UserController::class)->only(['edit', 'update', 'destroy', 'index', 'store']);
 Route::resource('roles', RoleController::class)->only(['edit', 'update', 'destroy', 'index', 'store']);
+Route::resource('login', LoginController::class)->only(['edit', 'update', 'destroy', 'index', 'store']);
 Route::resource('organitations', OrganitationController::class)->only(['edit', 'update', 'destroy', 'index', 'store']);
