35376d4faa11122463e272b5e794930cafe4ea72
rizkanfirmansyah <riezkanaprianda@gmail.com>
2023-07-22 01:31:50 +0700

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\OrganitationController;
+use App\Http\Controllers\ReporterController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\SafetyController;
 use App\Http\Controllers\UserController;
@@ -31,4 +32,5 @@ Route::resource('users', UserController::class)->only(['edit', 'update', 'destro
 Route::resource('organitations', OrganitationController::class)->only(['edit', 'update', 'destroy', 'index', 'store'])->middleware('auth');
 Route::resource('roles', RoleController::class)->only(['edit', 'update', 'destroy', 'index', 'store'])->middleware('auth');
 Route::resource('safeties', SafetyController::class)->only(['edit', 'update', 'destroy', 'index', 'store'])->middleware('auth');
+Route::resource('reporter', ReporterController::class)->only(['edit', 'update', 'destroy', 'index', 'store'])->middleware('auth');
 Route::post('/download/{path}/{filename}', [SafetyController::class, 'download'])->name('safeties.download');
